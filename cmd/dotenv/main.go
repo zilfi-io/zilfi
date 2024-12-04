64ab1eb3a6120baefd21674854755e3da4a9b31e
Joaquin Roca <joaquin.roca@gmail.com>
2024-12-03 21:56:31 -0500

    feat: Create CLI

--- cmd/dotenv/main.go
+++ cmd/dotenv/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "envy",
+}
+
+var initCmd = &cobra.Command{
+	Use: "init",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Not implemented")
+	},
+}
+
+var parseEnvCmd = &cobra.Command{
+	Use:   "import",
+	Short: "Import a .env file",
+	Long:  "Import a .env file into your environment",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Grab a .env file and parse it
+		location, _ := cmd.Flags().GetString("location")
+		parseFile(location)
+	},
+}
+
+func parseFile(location string) {
+	fmt.Printf("Parsing %s\n", location)
+}
+
+var helloCmd = &cobra.Command{
+	Use: "hello",
+	Run: func(cmd *cobra.Command, args []string) {
+		flags := cmd.Flags()
+		printFlags(*flags)
+	},
+}
+
+func printFlags(flags pflag.FlagSet) {
+	flags.VisitAll(func(flag *pflag.Flag) {
+		fmt.Printf("%s: %s\n", flag.Name, flag.Value)
+	})
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	helloCmd.Flags().StringP("env", "e", "dev", "Your environment")
+	helloCmd.Flags().StringP("name", "n", "world", "Your name")
+
+	parseEnvCmd.Flags().StringP("location", "l", ".env", "Location of the .env file")
+
+	rootCmd.AddCommand(helloCmd)
+	rootCmd.AddCommand(parseEnvCmd)
+	rootCmd.Execute()
+}
+
+func parseLine(line string) (key, value string) {
+	// Split the line into key and value
+	// KEY=VALUE
+	parts := strings.Split(line, "=")
+	if len(parts) == 2 {
+		return parts[0], parts[1]
+	}
+	return "", ""
 }
