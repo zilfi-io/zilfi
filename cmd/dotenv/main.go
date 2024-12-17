e60614d7945d2eb4f979f68d3695edc246e1f4b5
JoaquinRoca <joaquin.roca@gmail.com>
2024-12-16 23:05:57 -0500

    Parse env file to json

--- cmd/dotenv/main.go
+++ cmd/dotenv/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -25,12 +28,81 @@ var parseEnvCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// Grab a .env file and parse it
 		location, _ := cmd.Flags().GetString("location")
-		parseFile(location)
+		envContents, err := parseFile(location)
+		if err != nil {
+			fmt.Println("Error parsing file: ", err)
+			os.Exit(1)
+		}
+		writeEnv(envContents)
 	},
 }
 
-func parseFile(location string) {
+func countLinesWithContent(lines []string) int {
+	count := 0
+	for _, line := range lines {
+		if line != "" {
+			count++
+		}
+	}
+	return count
+}
+
+func writeEnv(contents string) {
+	fmt.Println("Writing to .env")
+	file, err := os.Create(".env.json")
+	if err != nil {
+		fmt.Println("Error creating file: ", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// Create a JSON file where each line is a key value pair
+	lines := strings.Split(contents, "\n")
+	var completed int
+	// Find the number of lines that are not empty
+	linesWithContent := countLinesWithContent(lines)
+
+	file.WriteString("{\n")
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) < 2 {
+			continue
+		}
+
+		key := strings.Trim(parts[0], "\"")
+		value := strings.Trim(parts[1], "\"")
+
+		if completed == linesWithContent-1 {
+			file.WriteString(fmt.Sprintf("  \"%s\": \"%s\"\n", key, value))
+			break
+		}
+		file.WriteString(fmt.Sprintf("  \"%s\": \"%s\",\n", key, value))
+		completed++
+	}
+	file.WriteString("}\n")
+}
+
+func parseFile(location string) (string, error) {
 	fmt.Printf("Parsing %s\n", location)
+	file, err := os.Open(location)
+	if err != nil {
+		fmt.Println("error opening file: ", err)
+		return "", err
+	}
+	defer file.Close()
+
+	// Read the file
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("error reading file: ", err)
+		return "", err
+	}
+	return string(content), nil
 }
 
 var helloCmd = &cobra.Command{
@@ -52,6 +124,7 @@ func main() {
 	helloCmd.Flags().StringP("name", "n", "world", "Your name")
 
 	parseEnvCmd.Flags().StringP("location", "l", ".env", "Location of the .env file")
+	parseEnvCmd.Flags().StringP("fileName", "f", ".env.json", "The name of the file to write to")
 
 	rootCmd.AddCommand(helloCmd)
 	rootCmd.AddCommand(parseEnvCmd)
